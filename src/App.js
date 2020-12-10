cf89e6e792b027672c98468607679d4e6a7ff293
YuraWin <YuraWin@ukr.net>
2020-12-10 17:28:22 +0200

    end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import Painting from './components/Painting';
+import PaintingList from './components/PaintingList';
+import Section from './components/Section';
 import paintings from './paintings.json';
-
 
 export default function App() {
   return (
     <div>
-      <Painting
+      <Section title="ТОп недели">
+        <PaintingList items={paintings} />
+      </Section>
+
+      <Section />
+    </div>
+  );
+}
+
+/* <Painting
         imageUrl={paintings[0].url}
         title={paintings[0].title}
         author={paintings[0].author.tag}
         profileUrl={paintings[0].author.url}
         price={paintings[0].price}
-      /> 
-     
-     <Painting
+        quantity={paintings[0].quantity}
+      />
+
+      <Painting
         imageUrl={paintings[1].url}
         title={paintings[1].title}
         author={paintings[1].author.tag}
         profileUrl={paintings[1].author.url}
         price={paintings[1].price}
-      /> 
+        quantity={paintings[1].quantity}
+      />
 
       <Painting
         imageUrl={paintings[2].url}
@@ -27,6 +38,6 @@ export default function App() {
         author={paintings[2].author.tag}
         profileUrl={paintings[2].author.url}
         price={paintings[2].price}
+        quantity={paintings[2].quantity}
       /> 
-    </div>)
-}
+      */
--- src/components/Painting.js
+++ src/components/Painting.js
@@ -1,23 +1,33 @@
 import PropTypes from 'prop-types';
-import defaultImage from './default.jpg'
+import defaultImage from './default.jpg';
 
-export default function Painting({ imageUrl =defaultImage, title, profileUrl, author = 'не известен', price }) {
-    return (
+export default function Painting({
+  imageUrl = defaultImage,
+  title,
+  profileUrl,
+  author = 'не известен',
+  price,
+  quantity,
+}) {
+  return (
     <div>
-        <img src={imageUrl} alt={title} width="480" />
-        <h2>{title}</h2>
-        <p>Автор: <a href={profileUrl}>{author}</a></p>
-        <p>Цена: {price} кредитов</p>
-        <p>Доступность: заканчивается или есть в наличии</p>
-        <button type="button">Добавить в корзину</button>
+      <img src={imageUrl} alt={title} width="480" />
+      <h2>{title}</h2>
+      <p>
+        Автор: <a href={profileUrl}>{author}</a>
+      </p>
+      <p>Цена: {price} кредитов</p>
+      <p>Доступность: {quantity > 5 ? 'есть в наличии' : 'заканчивается'}</p>
+      <button type="button">Добавить в корзину</button>
     </div>
-    );
+  );
 }
 
-Painting.propTypes= {
-    imadeUrl: PropTypes.string,
-    title: PropTypes.string,
-    profileUrl: PropTypes.string,
-    author: PropTypes.string,
-    price:PropTypes.number,
-}
+Painting.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  profileUrl: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  quantity: PropTypes.number.isRequired,
+};
