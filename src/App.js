a078524d73bfeda880d104fc31ad1a3c10769341
YuraWin <YuraWin@ukr.net>
2020-12-15 17:49:17 +0200

    Вернул всё взад App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import PaintingList from './components/PaintingList';
 import Section from './components/Section';
-// import paintings from './paintings.json';
+import paintings from './paintings.json';
 
 export default function App() {
   return (
     <div>
-      <h1>Привет</h1>
       <Section title="ТОп недели">
         <PaintingList items={paintings} />
       </Section>
